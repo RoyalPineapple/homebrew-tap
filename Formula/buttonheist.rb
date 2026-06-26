6c139f45562be9e1228fb3d8871ba36cc05043ca
Alex Odawa <aodawa@mac.com>
2026-06-26 15:07:53 +0000

    Update buttonheist to 0.6.5

--- Formula/buttonheist.rb
+++ Formula/buttonheist.rb
@@ -10,14 +10,14 @@
 class Buttonheist < Formula
   desc "Give AI agents full programmatic control of iOS apps"
   homepage "https://github.com/RoyalPineapple/TheButtonHeist"
-  version "0.6.4"
+  version "0.6.5"
 
-  url "https://github.com/RoyalPineapple/TheButtonHeist/releases/download/v0.6.4/buttonheist-0.6.4-macos.tar.gz"
-  sha256 "21d4a98fa65390962aadb9f35dbcf899e97d5cd257be2db60b8e68b50706aaf4"
+  url "https://github.com/RoyalPineapple/TheButtonHeist/releases/download/v0.6.5/buttonheist-0.6.5-macos.tar.gz"
+  sha256 "30a38574bb9a9b8e49fd03146a296e294278a5950763824ad2d8952a9eed5d62"
 
   resource "mcp" do
-    url "https://github.com/RoyalPineapple/TheButtonHeist/releases/download/v0.6.4/buttonheist-mcp-0.6.4-macos.tar.gz"
-    sha256 "63d7bd57943dcb64c16b91652d843f635cad91e2e49cea4a7233c1215d91e896"
+    url "https://github.com/RoyalPineapple/TheButtonHeist/releases/download/v0.6.5/buttonheist-mcp-0.6.5-macos.tar.gz"
+    sha256 "c4672cf2d5d6fb9d0e5d006dd1f73f3f6bca804aeeffa25c70e68b3f9f3c18e2"
   end
 
   depends_on :macos
